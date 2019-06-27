dccb7e4918b34e215fbaf931343ee47e9c37cac1
Bertie <berniesnell@gmail.com>
2019-06-27 13:55:41 -0400

    Setting up basic connection

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use std::net::TcpListener;
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.01:7878").unwrap();
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        println!("Connection Established!");
+    }
 }
